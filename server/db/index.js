2141eef4f67c74b9ed80e30c62863a9b2f0b1fd3
Rajko Culibrk <rajko93culibrk@gmail.com>
2021-04-12 18:19:29 +0200

    deployment 6

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URl,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false
